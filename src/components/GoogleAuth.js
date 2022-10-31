76df42f530344f0bc238717be3a5817442ec6eb4
bidulini <abidabida@hotmail.co.uk>
2022-10-31 19:01:30 +0100

    Google Authentication

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { connect } from "react-redux";
+import { signIn, signOut } from "../actions";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "213297129703-t0f1c7bq996r484t50lmtrrommtt068l.apps.googleusercontent.com",
+          scope: "email",
+          plugin_name: "Streamy",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.onAuthChange(this.auth.isSignedIn.get());
+          //SETTING UP AN EVENT LISTENER
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+  //this function is called whenever an user status is changed
+  onAuthChange = (isSignedIn) => {
+    if (isSignedIn) {
+      this.props.signIn(this.auth.currentUser.get().getId());
+    } else {
+      this.props.signOut();
+    }
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
+  renderAuthButton() {
+    if (this.props.isSignedIn === null) {
+      return null;
+    } else if (this.props.isSignedIn) {
+      return (
+        <button onClick={this.onSignOutClick} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={this.onSignInClick} className="ui red google button">
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
+    }
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
